ea8bbc4d58e1d48a0004a8cfeef5589384a23b7c
Taylan Tutar <tylntutar@gmail.com>
2023-09-22 22:28:17 +0300

    default app

---
+++ client/Program.cs
@@ -0,0 +1,13 @@
+﻿
+using Grpc.Net.Client;
+using server;
+
+Console.WriteLine("App is starting");
+
+using var channel = GrpcChannel.ForAddress("http://localhost:5292");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = await client.SayHelloAsync(new HelloRequest { Name = "Taylan" });
+Console.WriteLine("Greeting: " + reply.Message);
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
